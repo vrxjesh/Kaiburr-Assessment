6ee385a3e8276a0dbdc08a3d8b0d1a1dff518161
Vrajesh <vrajesh.k03@gmail.com>
2025-02-22 20:56:54 +0530

    Initial commit - Kaiburr Task 1 setup

---
+++ src/main/java/com/kaiburr/task1/demo/KaiburrTask1Application.java
@@ -0,0 +1,13 @@
+package com.kaiburr.task1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiburrTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiburrTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaiburr/task1/demo/KaiburrTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiburr.task1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiburrTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
